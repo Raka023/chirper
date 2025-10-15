a60535f0aac551e02ac447c04be8c1df989b4495
RaZhK_ <rakaputrautama23@outlook.com>
2025-10-15 12:24:42 +0700

    chap-6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
